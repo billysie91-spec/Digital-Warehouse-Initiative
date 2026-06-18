8ec1b4037dcacb8d60b9ac5d091d377ffb295408
billysie91-spec <billysie91@gmail.com>
2026-06-18 09:58:49 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
-document.getElementById("processBtn").addEventListener("click", () => {
+document.getElementById("processBtn").addEventListener("click", async () => {
 
     const file =
         document.getElementById("pdfFile").files[0];
 
     if (!file) {
-
         alert("Please select a PDF file");
-
         return;
     }
 
     document.getElementById("status").innerHTML =
-        "PDF selected:<br>" + file.name;
+        "Reading PDF...";
+
+    try {
+
+        const buffer =
+            await file.arrayBuffer();
+
+        const pdf =
+            await pdfjsLib.getDocument({
+                data: buffer
+            }).promise;
+
+        document.getElementById("status").innerHTML =
+            `PDF Loaded<br>
+             Total Pages: ${pdf.numPages}`;
+
+    }
+    catch (err) {
+
+        console.error(err);
+
+        document.getElementById("status").innerHTML =
+            "Failed to read PDF";
+    }
 
 });
